af8aa93f1f153e8bbbe520477f43e6a06823d5da
Kawann <kawannmarcelo7@gmail.com>
2025-02-18 18:37:03 -0300

    feat: Implement user registration route with password validation, encryption, and email duplication check

---
+++ src/routes/auth.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.post('/user/register', async (req, res) => {
+    const {name, email, password, confirmPassword} = req.body;
+
+    if(user.password !== user.confirmPassword) {
+        return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    const userExists = await prisma.user.findUnique({ where: { email } });
+    if(userExists) {
+        return res.status(400).json({ message: 'User already taken' });
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        });
+        res.status(201).json({
+            message: 'User created',
+            user: { name: user.name, email: user.email }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error'});
+    }
+})
